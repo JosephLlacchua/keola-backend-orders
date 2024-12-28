465a40fede7cf59772870b25e478e91836bd3751
JosephLlacchua <142326204+JosephLlacchua@users.noreply.github.com>
2024-12-28 02:53:20 -0500

    initialize project

---
+++ src/main/java/com/backend/orders/KeolaBackendOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.backend.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeolaBackendOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeolaBackendOrdersApplication.class, args);
+	}
+
+}
